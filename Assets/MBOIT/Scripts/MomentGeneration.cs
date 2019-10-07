b3e8c1f9904a3b7bc25e87605b3670ce9e553637
kohakukun <paraprogramar@gmail.com>
2019-10-07 23:09:45 +0200

    Working

---
+++ Assets/MBOIT/Scripts/MomentGeneration.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+
+namespace UnityEngine.Experimental.Rendering.Universal
+{
+
+    public class MomentGeneration : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class MomentGenerationSettings
+        {
+            public string passTag = "MomentGenerationFeature";
+            public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+
+            public FilterSettings filterSettings = new FilterSettings();
+
+            public Material overrideMaterial = null;
+            public int overrideMaterialPassIndex = 0;
+
+            public bool overrideDepthState = false;
+            public CompareFunction depthCompareFunction = CompareFunction.LessEqual;
+            public bool enableWrite = true;
+
+            public StencilStateData stencilSettings = new StencilStateData();
+
+            public CustomCameraSettings cameraSettings = new CustomCameraSettings();
+        }
+
+        [System.Serializable]
+        public class FilterSettings
+        {
+            // TODO: expose opaque, transparent, all ranges as drop down
+            public RenderQueueType RenderQueueType;
+            public LayerMask LayerMask;
+            public string[] PassNames;
+
+            public FilterSettings()
+            {
+                RenderQueueType = RenderQueueType.Opaque;
+                LayerMask = 0;
+            }
+        }
+
+        [System.Serializable]
+        public class CustomCameraSettings
+        {
+            public bool overrideCamera = false;
+            public bool restoreCamera = true;
+            public Vector4 offset;
+            public float cameraFieldOfView = 60.0f;
+        }
+
+        public MomentGenerationSettings settings = new MomentGenerationSettings();
+
+        MomentGenerationPass MomentGenerationPass;
+
+        public override void Create()
+        {
+            FilterSettings filter = settings.filterSettings;
+            MomentGenerationPass = new MomentGenerationPass(settings.passTag, settings.Event, filter.PassNames,
+                filter.RenderQueueType, filter.LayerMask, settings.cameraSettings);
+
+            MomentGenerationPass.overrideMaterial = settings.overrideMaterial;
+            MomentGenerationPass.overrideMaterialPassIndex = settings.overrideMaterialPassIndex;
+
+            if (settings.overrideDepthState)
+                MomentGenerationPass.SetDetphState(settings.enableWrite, settings.depthCompareFunction);
+
+            if (settings.stencilSettings.overrideStencilState)
+                MomentGenerationPass.SetStencilState(settings.stencilSettings.stencilReference,
+                    settings.stencilSettings.stencilCompareFunction, settings.stencilSettings.passOperation,
+                    settings.stencilSettings.failOperation, settings.stencilSettings.zFailOperation);
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            MomentGenerationPass.Setup(ref renderingData);
+            renderer.EnqueuePass(MomentGenerationPass);
+        }
+    }
+}
+
---
+++ Assets/MBOIT/Scripts/MomentGenerationPass.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+
+namespace UnityEngine.Experimental.Rendering.Universal
+{
+    public class MomentGenerationPass : ScriptableRenderPass
+    {
+        RenderQueueType renderQueueType;
+        FilteringSettings m_FilteringSettings;
+        MomentGeneration.CustomCameraSettings m_CameraSettings;
+        string m_ProfilerTag;
+
+        public Material overrideMaterial { get; set; }
+        public int overrideMaterialPassIndex { get; set; }
+
+        List<ShaderTagId> m_ShaderTagIdList = new List<ShaderTagId>();
+
+        int m_MomentTextureWidth;
+        int m_MomentTextureHeight;
+        RenderTexture m_MomentTexture;
+        RenderTargetHandle m_MomentTextureHandle;
+
+
+
+        public void SetDetphState(bool writeEnabled, CompareFunction function = CompareFunction.Less)
+        {
+            m_RenderStateBlock.mask |= RenderStateMask.Depth;
+            m_RenderStateBlock.depthState = new DepthState(writeEnabled, function);
+        }
+
+        public void SetStencilState(int reference, CompareFunction compareFunction, StencilOp passOp, StencilOp failOp, StencilOp zFailOp)
+        {
+            StencilState stencilState = StencilState.defaultValue;
+            stencilState.enabled = true;
+            stencilState.SetCompareFunction(compareFunction);
+            stencilState.SetPassOperation(passOp);
+            stencilState.SetFailOperation(failOp);
+            stencilState.SetZFailOperation(zFailOp);
+
+            m_RenderStateBlock.mask |= RenderStateMask.Stencil;
+            m_RenderStateBlock.stencilReference = reference;
+            m_RenderStateBlock.stencilState = stencilState;
+        }
+
+        RenderStateBlock m_RenderStateBlock;
+
+        public MomentGenerationPass(string profilerTag, RenderPassEvent renderPassEvent, string[] shaderTags, RenderQueueType renderQueueType, int layerMask, MomentGeneration.CustomCameraSettings cameraSettings)
+        {
+            m_ProfilerTag = profilerTag;
+            this.renderPassEvent = renderPassEvent;
+            this.renderQueueType = renderQueueType;
+            this.overrideMaterial = null;
+            this.overrideMaterialPassIndex = 0;
+            RenderQueueRange renderQueueRange = (renderQueueType == RenderQueueType.Transparent)
+                ? RenderQueueRange.transparent
+                : RenderQueueRange.opaque;
+            m_FilteringSettings = new FilteringSettings(renderQueueRange, layerMask);
+
+            if (shaderTags != null && shaderTags.Length > 0)
+            {
+                foreach (var passName in shaderTags)
+                    m_ShaderTagIdList.Add(new ShaderTagId(passName));
+            }
+            else
+            {
+                m_ShaderTagIdList.Add(new ShaderTagId("UniversalForward"));
+                m_ShaderTagIdList.Add(new ShaderTagId("LightweightForward"));
+                m_ShaderTagIdList.Add(new ShaderTagId("SRPDefaultUnlit"));
+            }
+
+            m_RenderStateBlock = new RenderStateBlock(RenderStateMask.Nothing);
+            m_CameraSettings = cameraSettings;
+
+        }
+
+        public bool Setup(ref RenderingData renderingData)
+        {
+            m_MomentTextureHeight = renderingData.cameraData.cameraTargetDescriptor.height;
+            m_MomentTextureWidth = renderingData.cameraData.cameraTargetDescriptor.width;
+
+            m_MomentTextureHandle.Init("_MomentTexture");
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            m_MomentTexture = RenderTexture.GetTemporary(m_MomentTextureWidth, m_MomentTextureHeight, 0, GraphicsFormat.R32G32B32A32_SFloat);
+            ConfigureTarget(new RenderTargetIdentifier(m_MomentTexture));
+            ConfigureClear(ClearFlag.All, Color.black);
+        }
+
+        /// <inheritdoc/>
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            if (cmd == null)
+                throw new ArgumentNullException("cmd");
+
+            if (m_MomentTexture)
+            {
+                RenderTexture.ReleaseTemporary(m_MomentTexture);
+                m_MomentTexture = null;
+            }
+        }
+
+        void Clear()
+        {
+            m_MomentTexture = null;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            SortingCriteria sortingCriteria = (renderQueueType == RenderQueueType.Transparent)
+                ? SortingCriteria.CommonTransparent
+                : renderingData.cameraData.defaultOpaqueSortFlags;
+
+            DrawingSettings drawingSettings = CreateDrawingSettings(m_ShaderTagIdList, ref renderingData, sortingCriteria);
+            drawingSettings.overrideMaterial = overrideMaterial;
+            drawingSettings.overrideMaterialPassIndex = overrideMaterialPassIndex;
+
+            Camera camera = renderingData.cameraData.camera;
+            float cameraAspect = (float)camera.pixelWidth / (float)camera.pixelHeight;
+            CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+            using (new ProfilingSample(cmd, m_ProfilerTag))
+            {
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+
+                if (m_CameraSettings.overrideCamera)
+                {
+                    Matrix4x4 projectionMatrix = Matrix4x4.Perspective(m_CameraSettings.cameraFieldOfView, cameraAspect,
+                        camera.nearClipPlane, camera.farClipPlane);
+
+                    Matrix4x4 viewMatrix = camera.worldToCameraMatrix;
+                    Vector4 cameraTranslation = viewMatrix.GetColumn(3);
+                    viewMatrix.SetColumn(3, cameraTranslation + m_CameraSettings.offset);
+
+                    cmd.SetViewProjectionMatrices(viewMatrix, projectionMatrix);
+                    context.ExecuteCommandBuffer(cmd);
+                }
+
+                context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref m_FilteringSettings,
+                    ref m_RenderStateBlock);
+
+                cmd.SetGlobalTexture("_MomentTexture", m_MomentTexture);
+
+                if (m_CameraSettings.overrideCamera && m_CameraSettings.restoreCamera)
+                {
+                    Matrix4x4 projectionMatrix = Matrix4x4.Perspective(camera.fieldOfView, cameraAspect,
+                        camera.nearClipPlane, camera.farClipPlane);
+
+                    cmd.Clear();
+                    cmd.SetViewProjectionMatrices(camera.worldToCameraMatrix, projectionMatrix);
+                }
+            }
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+}
